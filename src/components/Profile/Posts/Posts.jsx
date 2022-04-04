1a1c6e656ccf0481e6fc41f04ab9523b657d3f47
niko <nikoguitar1995@gmail.com>
2022-04-04 16:13:08 +0300

    Create ref

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import classes from './Posts.module.css';
 import Post from "./Post/Post";
-const Posts = (props) => {
 
+const Posts = (props) => {
+    let newPostText = React.createRef();
+    const addPost = () => {
+        let newPost = newPostText.current.value;
+        alert(newPost);
+    }
     return (
         <div className={classes.posts}>
             <h1>My posts</h1>
             <form action="">
-                <textarea name="" id=""></textarea>
-                <button>Send</button>
+                <textarea ref={newPostText}></textarea>
+                <button onClick={addPost}>Send</button>
             </form>
             {props.postData.map(post => <Post message={post.message}/>)}
         </div>
